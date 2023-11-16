eee856e54995a4b45a285b48c4ce5fad65f9f99d
JOMG10 <116333088+JOMG10@users.noreply.github.com>
2023-11-16 00:20:36 -0600

    ..

---
+++ carpetas/clases.js
@@ -0,0 +1,23 @@
+class Usuario{
+    tipo='usuario';
+
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+
+        console.log('nuevo usuario registrado   ')
+    }
+
+    obtenerNombreCompleto(){
+        console.log('obteniendo datos')
+        return `${this.nombre} ${this.apellido}`
+    }
+}
+
+const usuario = new Usuario ('genaro','jose');
+const nombreCompleto = usuario.obtenerNombreCompleto();
+console.log(nombreCompleto);
+
+//const usuario2 = new Usuario ('genaro','jose');
+
+// Usuario objUsuario = new Usuario();
