e589bb357dc87fd5d3e065aa4e20b2b868a8c620
JOMG10 <116333088+JOMG10@users.noreply.github.com>
2023-12-13 23:29:14 -0600

    ..

--- carpetas/Crud.js
+++ carpetas/Crud.js
@@ -1,25 +1,25 @@
-const correo = document.querySelector('#formulario [name = "correo" ]').value
+// const boton = document.getElementById('boton');
+
+// boton.addEventListener('click',(e)=>{
+//     e.preventDefault()
+//    // console.log(document.forms['formulario']['correo'].value)
+//     const correo = document.querySelector('#correo').value
+//     const prueba =  document.getElementById('correo').value
+
+//     console.log(prueba)
+// })
+
+const boton1 = document.getElementById('calcular')
+boton1.addEventListener('click', (e) =>{
+    e.preventDefault()
+    const numero1 = document.getElementById('numero1').value
+    const numero2 = document.getElementById('numero2').value
+   
+    const calcularSuma = numero1 + numero2     
+
+    console.log(calcularSuma)
 
 
-console.log(document.forms['formulario']['correo'])
 
 
-
-// const objeto= {
-//     nombre : "",
-//     apellido:""
-// }
-
-
-// var arreglo = mutablelistOf<objeto>(any)
-
-// const array = [] = arreglo [
-//     {
-//         nombre:"jose",
-//         apellido:"martinez"
-        
-//     }
-// ]
-
-// console.log(array)
-// arreglo,nombre 
+})
